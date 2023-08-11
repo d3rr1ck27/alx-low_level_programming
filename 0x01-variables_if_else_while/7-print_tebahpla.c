2e18e93ec46c968397d71766f8c50982b4bf9edf
d3rr1ck27 <derricklove2704@gmail.com>
2023-08-11 10:06:25 -0700

    Task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main-outputs alphabet in reverse
+ * Return: Always 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; cd >= 'a': ch--)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
