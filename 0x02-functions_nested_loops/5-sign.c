c58f9f167f5a66b64a323ea69c3c2af7306e01af
d3rr1ck27 <derricklove2704@gmail.com>
2023-08-15 04:59:57 -0700

    Task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the operator sign of number
+ * @n: the int to check
+ * Return: 1 and prints + if n >  zero
+ * 0 and prints 0 if n == zer0
+ * -1 and prints - if n < zero
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	} else if (n == 0)
+}
