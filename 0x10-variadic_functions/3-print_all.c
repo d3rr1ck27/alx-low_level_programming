51f98503ffff73864e1c301230b088f004694d47
d3rr1ck27 <derricklove2704@gmail.com>
2023-09-14 07:16:14 -0700

    Task 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -33,6 +33,7 @@ void print_all(const char * const format, ...)
 					str = va_arg(list, char *);
 					if (!str)
 						str = "(nil)";
+					printf("%s%s", sep, str);
 					break;
 				default:
 					i++;
